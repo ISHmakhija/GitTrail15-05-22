8836a4d7ec5348e00eb1c86a76f8716bff519d9c
ISHmakhija <raj372721@gmail.com>
2022-05-15 11:37:49 +0100

    AddMethod

---
+++ src/AdditionMethod.java
@@ -0,0 +1,15 @@
+public class AdditionMethod
+
+{
+
+    public static void add (int a,int b) {
+        System.out.println(a+b);
+    }
+    public static void main(String[] args)
+    {
+        add(10,12);
+
+    }
+
+
+}
